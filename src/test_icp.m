1d48a80bfd98b5b07b59a5e4e217ccd8278785ee
Elon <zqsh19970218@gmail.com>
2018-04-28 17:02:05 +0800

    copy file!

--- src/test_icp.m
+++ src/test_icp.m
@@ -1,5 +1,5 @@
 %% this is the test file of of icp
-load data/icpTestData.mat
+load ../data/icpTestData.mat
 result = icp(points2D1,points2D2,points3D1,points3D2,@projection);
 result;
 disp('Answer')
