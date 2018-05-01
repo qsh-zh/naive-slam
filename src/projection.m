6abd59790e54a4a9df872695d6f09fac26520ffc
Elon <zqsh19970218@gmail.com>
2018-05-01 11:30:00 +0800

    Elon fix bug in the test icp and imrove the projection,m to the cluster of points instead of a single point.

--- src/projection.m
+++ src/projection.m
@@ -1,15 +1,25 @@
-function point2D = projection(point3D)
+function point2D = projection(points3D)
 %myFun - Description
 %
 % Syntax: points2D = projection(points3D)
 % point2D form (u,v,1) in picture
 % point3D form (x,y,z,1) in real world
 % Long description
+
+% 2018.5.1 elon edit the form to the high-dimension situation!
+%size(points2D) n*3
+%size(points3D) n*4
+    if size(points3D,2) ~=4
+        disp('input error, size(points3D,2) should be 4!')
+    end
     calibname = '../data/calib.txt';
     T = readtable(calibname, 'Delimiter', 'space', 'ReadRowNames', true, 'ReadVariableNames', false);
     A = table2array(T);
     P1 = vertcat(A(1,1:4), A(1,5:8), A(1,9:12));
-    point2D = P1 * point3D;
-    point2D = point2D / point2D(3);
-
+    points3D = points3D';
+    points2D = P1 * points3D;
+    point2D = points2D';
+    point2D(:,1) = point2D(:,1) ./ point2D(:,3);
+    point2D(:,2) = point2D(:,2) ./ point2D(:,3);
+    point2D(:,3) = point2D(:,3) ./ point2D(:,3);
 end
--- src/test_icp.m
+++ src/test_icp.m
@@ -1,5 +1,10 @@
 %% this is the test file of of icp
 load ../data/icpTestData.mat
+points2D1 = [points2D1,ones(size(points2D1,1),1)];
+points2D2 = [points2D2,ones(size(points2D1,1),1)];
+points3D1 = [points3D1,ones(size(points2D1,1),1)];
+points3D2 = [points3D2,ones(size(points2D1,1),1)];
+
 result = icp(points2D1,points2D2,points3D1,points3D2,@projection);
 result;
 disp('Answer')
