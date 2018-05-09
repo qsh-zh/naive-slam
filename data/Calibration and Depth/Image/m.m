5e49c964180a413d77879c5df0b46984bc3aea34
Minghao Gou <gmh2015@163.com>
2018-05-09 20:09:06 +0800

    add data

---
+++ data/Calibration and Depth/Image/m.m
@@ -0,0 +1,10 @@
+function y = m(i,f)
+    f = num2str(f(i));
+    name = strcat('20180512_14410',f,'A.jpg')
+    im = imread(name);
+    imL = im(:,1:2240,:);
+    imR = im(:,2241:end,:);
+    imwrite(imL,strcat('./L/',f,'L.jpg'))
+    imwrite(imR,strcat('./R/',f,'R.jpg'))
+    fprintf('over')
+end
